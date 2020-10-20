4ca7cc447c3e283e839360fa771f8a7752ae86ba
cnoriega20 <c79.noriega@gmail.com>
2020-10-20 01:15:28 -0500

    Initial commit

---
+++ src/main/java/com/msvcs/eureka/namingserver/CnEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.msvcs.eureka.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnEurekaNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnEurekaNamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msvcs/eureka/namingserver/CnEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvcs.eureka.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnEurekaNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
